620efce83f0a50d54929b31670df4c242b3e8f16
MrRafalSzymanski <mr.rafal.szymanski@outlook.com>
2015-09-14 14:48:42 +0200

    The "$on" call should be assigned to a variable, in order to be destroyed during the $destroy event

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -7,7 +7,7 @@ angular.module('ngDevstack')
     // handling UI Bootstrap Collapse plugin
     $scope.isCollapsed = true;
 
-    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+    var stateChangeSuccessEvent = $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         if (angular.isDefined(toState.data.pageTitle)) {
             $scope.pageTitle = toState.data.pageTitle + ' | ng-devstack';
         }
