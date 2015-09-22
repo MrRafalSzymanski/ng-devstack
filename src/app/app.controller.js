9cc76fcd46c493b7024cb788c5648484e0c9a5f6
Szymański,  Rafał <rafal.szymanski@mobica.com>
2015-09-22 12:24:23 +0200

    Transformed direct $scope usage with inner variable. Additionally tranformed one direct controller with 'as' alias.

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,14 +1,13 @@
 angular.module('ngDevstack')
 
 .controller('AppController', function ($rootScope, $scope) {
-    'use strict';
-
     // handling UI Bootstrap Collapse plugin
-    $scope.isCollapsed = true;
+    var vm = this;
+    vm.isCollapsed = true;
 
     var stateChangeSuccessEvent = $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         if (angular.isDefined(toState.data.pageTitle)) {
-            $scope.pageTitle = toState.data.pageTitle + ' | ng-devstack';
+            vm.pageTitle = toState.data.pageTitle + ' | ng-devstack';
         }
     });
 });
